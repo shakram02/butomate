3b1fe4116d40639224190a13823dde994c56bff6
ahmed <ahmedhamdau@gmail.com>
2017-06-03 04:45:09 +0200

    add tamplate config, script  folder

---
+++ bot/src/config.template.ts
@@ -0,0 +1,8 @@
+import {BotConfig} from './bot_config';
+
+export const config: BotConfig = {
+  appId: 'appId',   // Microsoft app id
+  appPassword: 'password',  // Microsoft app password
+  pageKiteHandle: '***.pagekite.me', // Your handle
+  portNumber: 0  // Port number
+};
