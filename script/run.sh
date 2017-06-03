b30d20309ed50196e055d38ba1b89bd653acae2d
ahmed <ahmedhamdau@gmail.com>
2017-06-03 11:19:51 +0200

    change build directory

--- script/run.sh
+++ script/run.sh
@@ -2,8 +2,8 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 BOLD='\033[1m'
 
-mkdir bot/src/build
+mkdir bot/build
 echo -e "${YELLOW}${BOLD}Building project..."
-tsc -p ./bot/src/ --pretty
+tsc -p ./bot/src/ --pretty --outDir "./bot/build"
 echo -e "Starting project... ${NC}"
-node bot/src/build/bot.js
+node bot/build/bot.js
