fe1c45055636b3ecd0a612670b03a87b7a472c6f
ahmed <ahmedhamdau@gmail.com>
2017-06-03 04:48:15 +0200

    functions for bootstrap

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -2,6 +2,19 @@
 cd bot
 
 # Create an empty config file
+create_config
+
+# Install dependencies
+npm install
+
+# Reminder
+config_reminder
+
+# Return to parent directory and run the code
+cd ..
+./run.sh
+
+function create_config{
 touch src/config.ts
 echo "import {BotConfig} from './bot_config';
 
@@ -12,15 +25,11 @@ export const config: BotConfig = {
   portNumber: 0000  // Port number
 };
 " >> config.ts
+}
 
-# Install dependencies
-npm install
-
-# Reminder
+function config_reminder{
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 BOLD='\033[1m'
-echo -e "${YELLOW} ${BOLD} DONOT forget to setup your config.ts file${NC} the tutorial in README will help"
-# Return to parent directory and run the code
-cd ..
-./run.sh
+echo -e "${YELLOW} ${BOLD} DONOT forget to setup your config.ts file${NC} the tutorial in README will help"  
+}
